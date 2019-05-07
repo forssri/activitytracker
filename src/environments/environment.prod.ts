3762488d673e5f98b26f16c8a77ec06aaa5960c4
Sriram Sureshkumar <sriram.sureshkumar@viacomcontractor.com>
2019-05-07 19:17:51 +0530

    Updated environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDpqD5hI02T8Z70eG-EXazmdJG1dQUyMrY',
+    authDomain: 'activitytracker-101.firebaseapp.com',
+    databaseURL: 'https://activitytracker-101.firebaseio.com',
+    projectId: 'activitytracker-101',
+    storageBucket: 'activitytracker-101.appspot.com',
+    messagingSenderId: '124655625083',
+    appId: '1:124655625083:web:8c975b83d1342299'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDpqD5hI02T8Z70eG-EXazmdJG1dQUyMrY',
+    authDomain: 'activitytracker-101.firebaseapp.com',
+    databaseURL: 'https://activitytracker-101.firebaseio.com',
+    projectId: 'activitytracker-101',
+    storageBucket: 'activitytracker-101.appspot.com',
+    messagingSenderId: '124655625083',
+    appId: '1:124655625083:web:8c975b83d1342299'
+  }
 };
 
 /*
